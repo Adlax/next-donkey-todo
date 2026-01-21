3d62dd8eda509eef0aa8f35a711a8443678520da
martine <h.videlier@gmail.com>
2026-01-21 20:31:21 +0100

    Create todo back ok

---
+++ app/api/create-todo/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { title, date } = body;
+  const post = await prisma.todo.create({
+    data: {
+      title,
+      date,
+    },
+  });
+  return NextResponse.json(post, { status: 201 });
+}
--- app/create-todo/page.tsx
+++ app/create-todo/page.tsx
@@ -1,61 +1,101 @@
 "use client";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
-
-const handleCreate = async (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-};
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const page = () => {
+  const router = useRouter();
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
   const [isDisabled, setIsDisabled] = useState(false);
 
+  const notifyError = () => toast.error("Fill all the fields please");
+  const notifySuccess = () =>
+    toast.success("Todo created succesfully", {
+      onClose: () => router.push("/todos"),
+    });
+
+  const handleCreateTodo = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!title || !date) {
+      notifyError();
+      return;
+    }
+    setIsDisabled(true);
+    const response = await fetch(`/api/create-todo`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, date }),
+    });
+    if (response.ok) {
+      notifySuccess();
+    } else {
+      alert("Error while creating the todo");
+    }
+  };
+
   return (
-    <form className="form" onSubmit={handleCreate}>
-      <div className="title">
-        <h1>Create a Todo</h1>
-      </div>
-      <div className="align-horizontal">
-        <div className="todo-container">
-          <label htmlFor="" className="placeholder">
-            Tache
-          </label>
-          <input
-            type="text"
-            name=""
-            id=""
-            className="input"
-            placeholder="Ecrivez le todo ici"
-            autoComplete="off"
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-            value={title}
-          />
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        pauseOnHover
+        theme="light"
+      />
+      <form className="form" onSubmit={handleCreateTodo}>
+        <div className="title">
+          <h1>Create a Todo</h1>
         </div>
-        <div className="date-container">
-          <label htmlFor="" className="placeholder">
-            Date
-          </label>
-          <input
-            type="date"
-            placeholder="DD/MM/YYYY"
-            className="input"
-            value={date}
-            onChange={(e) => {
-              setDate(e.target.value);
-            }}
-          />
+        <div className="align-horizontal">
+          <div className="todo-container">
+            <label htmlFor="" className="placeholder">
+              Tache
+            </label>
+            <input
+              type="text"
+              name=""
+              id=""
+              className="input"
+              placeholder="Ecrivez le todo ici"
+              autoComplete="off"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+              value={title}
+            />
+          </div>
+          <div className="date-container">
+            <label htmlFor="" className="placeholder">
+              Date
+            </label>
+            <input
+              type="date"
+              placeholder="DD/MM/YYYY"
+              className="input"
+              value={date}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <div className="button-container">
-        {!isDisabled && (
-          <button type="submit" className="btn-success">
-            Creer
-          </button>
-        )}
-      </div>
-    </form>
+        <div className="button-container">
+          {!isDisabled && (
+            <button type="submit" className="btn-success">
+              Creer
+            </button>
+          )}
+        </div>
+      </form>
+    </>
   );
 };
 
